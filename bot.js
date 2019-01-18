7b439e65e41804b90541354d0ce02561fd42ec0b
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-18 20:45:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`FireHost|-bc`,"http://twitch.tv/Death Shop")
 });
 
 client.on('message', message => {
